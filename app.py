17445039eb3e62b22664b05ed3caa4c9e6bd13e3
Francis Tsang <frtsang40@gmail.com>
2019-11-04 14:39:22 -0800

    hw2

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/fortune')
+@app.route('/fortune', methods=['GET', 'POST'])
 def fortune():
     return render_template('fortune_form.html')
 
 @app.route('/fortune_results')
 def fortune_results():
-    user_
+    name = request.args.get('name')
+    food = request.args.get('food')
+    petlist = request.args.get('petlist')
+    age = request.args.get('age')
+
+    fortune = {
+        'name' : name,
+        'food' : food,
+        'petlist' : petlist,
+        'age' : age
+    }
+
+    return render_template('results.html', fortune=fortune)
+if __name__ == "__main__":
+    app.run()
